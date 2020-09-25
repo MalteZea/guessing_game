95f9bbc1ef8464e0807cc095832a6084ec5f3199
MalteZea <70883688+MalteZea@users.noreply.github.com>
2020-09-25 11:22:40 +0200

    Update main.rs
    updated with own input

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,51 @@ use std::cmp::Ordering;
 
 fn main() {
 
-    println!("Guess the number!");
+    println!("Guess a letter!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut selectedWord = ""
+
+    let secretNumber = rand::thread_rng().gen_range(1, 6); //Lowest number is inclusive, higest is exclusive
+
+    match secretNumber {
+        1 => selectedWord = "Keyboard",
+        2 => selectedWord = "Rust",
+        3 => selectedWord = "Space",
+        4 => selectedWord = "Java",
+        5 => selectedWord = "Quarentine",
+    }
+
+
+    // struct Letter {
+    //     character: char, 
+    //     revealed: bool,
+    // }
 
     loop {
 
         println!("Please input your guess.");
 
         let mut guess = String::new();
-        
-        // if guess == "exit" {
-        //    break;
-        // }
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+
+            // let guess: u32 = match guess.trim().parse() {
+            //     Ok(num) => num,
+            //     Err(_) => continue,
+            // };
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number) {
+        match guess.cmp(&secretNumber) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
-            }
+            } 
         }
+
     }
 }
